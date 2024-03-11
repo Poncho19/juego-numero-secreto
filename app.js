402ff86390ff3c01c2d43d3cb0ff1ed02917db9e
Alfonso Rafael <alfonsorafaelcamacho@gmail.com>
2024-03-11 17:51:08 -0600

    02 comentarios y else

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un numero porfavor: ");
+let numeroUsuario = prompt("Me indicas un numero entre 1 y 10 porfavor: ");
 
 console.log(numeroUsuario);
 
+//Este codigo realiza la comparacion
 if(numeroUsuario == numeroSecreto){
+    //Acertamos, fue verdadera la condicion
     alert('Acertaste el numero');
+}else{
+    //No se cumplio la condicion
+    alert("Lo siento no acertaste el numero");
 }
