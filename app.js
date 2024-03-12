08e8aa248b6b33b5676bb7d5cdb1d3c54ac8c7c1
Alfonso Rafael <alfonsorafaelcamacho@gmail.com>
2024-03-11 18:46:33 -0600

    04 Contador intentos

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 //Declaracion de variables
 let numeroSecreto = 8;
 let numeroUsuario = 0;
+let intentos = 1;
+let palabraVeces = 'vez';
+
 
 while(numeroUsuario != numeroSecreto){
     numeroUsuario = prompt("Me indicas un numero entre 1 y 10 porfavor: ");
@@ -10,14 +13,18 @@ while(numeroUsuario != numeroSecreto){
     //Este codigo realiza la comparacion
     if(numeroUsuario == numeroSecreto){
         //Acertamos, fue verdadera la condicion
-        alert(`Acertaste, el numero ${numeroUsuario}`);
+        alert(`Acertaste, el numero ${numeroUsuario}, Lo hiciste en ${intentos} ${palabraVeces}`);
     }else{
+        //No se cumplio la condicion
+        alert("Lo siento no acertaste el numero");
         if(numeroUsuario > numeroSecreto){
             alert("El numero secreto es menor");
         }else{
             alert("El numero secreto es mayor");
         }
-        //No se cumplio la condicion
-        alert("Lo siento no acertaste el numero");
+        //Incrementamos el contador cuando no se acierta
+        intentos += 1;
+        palabraVeces = "veces";
+        
     }
 }
