96fcf02c3e1a92cced63e466829fe465e09a7b60
Alfonso Rafael <alfonsorafaelcamacho@gmail.com>
2024-03-11 20:04:21 -0600

    08 math.random() en nuestro codigo

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 //Declaracion de variables
-let numeroSecreto = 8;
+let numeroSecreto = Math.floor(Math.random()*10)+1;
 let numeroUsuario = 0;
 let intentos = 1;
 let maximosIntentos = 3;
 
+console.log("El numero secreto es: " + numeroSecreto);
+
 
 while(numeroUsuario != numeroSecreto){
-    numeroUsuario = prompt("Me indicas un numero entre 1 y 10 porfavor: ");
+    numeroUsuario = parseInt(prompt("Me indicas un numero entre 1 y 10 porfavor: "));
+    console.log(typeof(numeroUsuario));
 
-    console.log(numeroUsuario);
+    console.log(`El numero de usuario es: ${numeroUsuario}`);
 
     //Este codigo realiza la comparacion
     if(numeroUsuario == numeroSecreto){
         //Acertamos, fue verdadera la condicion
-        alert(`Acertaste, el numero ${numeroUsuario}, Lo hiciste en ${intentos} ${intentos > 1? "vez":"veces"}`);
+        alert(`Acertaste el numero es: ${numeroUsuario}, Lo hiciste en ${intentos} ${intentos == 1? "intento":"intentos"}`);
     }else{
         //No se cumplio la condicion
-        alert("Lo siento no acertaste el numero");
         if(numeroUsuario > numeroSecreto){
-            alert("El numero secreto es menor");
+            alert("Lo siento no acertaste el numero, el numero secreto es menor");
         }else{
-            alert("El numero secreto es mayor");
+            alert("Lo siento no acertaste el numero, el numero secreto es mayor");
         }
         if(intentos == maximosIntentos){
             alert(`Llegaste al numero maximo de intentos:  ${intentos}`);
